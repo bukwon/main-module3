2f1182c1216f5ea8db6e67882e431eaee280030e
bukwon <dnu05043@gmail.com>
2024-09-13 13:29:49 +0900

    mainmodule3

---
+++ src/main/java/com/example/mainmodule3/Mainmodule3Application.java
@@ -0,0 +1,13 @@
+package com.example.mainmodule3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mainmodule3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mainmodule3Application.class, args);
+	}
+
+}
